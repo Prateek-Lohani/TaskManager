33e958c6e5994054a00451e78618ab2b85ec9962
Prateek Lohani <67368888+Prateek-Lohani@users.noreply.github.com>
2024-02-22 13:56:22 +0530

    Update index.jsx

--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -1,31 +1,41 @@
-import React, { useState } from 'react'
-import { Navigate, Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../../contexts/authContext'
-import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth'
+import React, { useState } from 'react';
+import { Navigate, Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../contexts/authContext';
+import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth';
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isRegistering, setIsRegistering] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const navigate = useNavigate()
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setconfirmPassword] = useState('')
-    const [isRegistering, setIsRegistering] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
-
-    const { userLoggedIn } = useAuth()
+    const { userLoggedIn } = useAuth();
 
     const onSubmit = async (e) => {
-        e.preventDefault()
-        if(!isRegistering) {
-            setIsRegistering(true)
-            await doCreateUserWithEmailAndPassword(email, password)
+        e.preventDefault();
+        if (!isRegistering) {
+            if (!email.includes('@') || !email.includes('.com')) {
+                setErrorMessage('Invalid email');
+            } else if (password.length < 8) {
+                setErrorMessage('Password must be at least 8 characters long');
+            } else if (password !== confirmPassword) {
+                setErrorMessage('Confirm password does not match');
+            } else {
+                setIsRegistering(true);
+                try {
+                    await doCreateUserWithEmailAndPassword(email, password);
+                } catch (error) {
+                    setErrorMessage(error.message);
+                }
+            }
         }
-    }
+    };
 
     return (
         <>
-            {userLoggedIn && (<Navigate to={'/home'} replace={true} />)}
+            {userLoggedIn && <Navigate to={'/home'} replace={true} />}
 
             <main className="w-full h-screen flex self-center place-content-center place-items-center">
                 <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
@@ -33,58 +43,46 @@ const Register = () => {
                         <div className="mt-2">
                             <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
                         </div>
-
                     </div>
-                    <form
-                        onSubmit={onSubmit}
-                        className="space-y-4"
-                    >
+                    <form onSubmit={onSubmit} className="space-y-4">
                         <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Email
-                            </label>
+                            <label className="text-sm text-gray-600 font-bold">Email</label>
                             <input
                                 type="email"
                                 autoComplete='email'
                                 required
-                                value={email} onChange={(e) => { setEmail(e.target.value) }}
+                                value={email}
+                                onChange={(e) => { setEmail(e.target.value) }}
                                 className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
                             />
                         </div>
-
                         <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Password
-                            </label>
+                            <label className="text-sm text-gray-600 font-bold">Password</label>
                             <input
                                 disabled={isRegistering}
                                 type="password"
-                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                                 autoComplete='new-password'
                                 required
-                                value={password} onChange={(e) => { setPassword(e.target.value) }}
+                                value={password}
+                                onChange={(e) => { setPassword(e.target.value) }}
                                 className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
                             />
                         </div>
-
                         <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Confirm Password
-                            </label>
+                            <label className="text-sm text-gray-600 font-bold">Confirm Password</label>
                             <input
                                 disabled={isRegistering}
                                 type="password"
                                 autoComplete='off'
                                 required
-                                value={confirmPassword} onChange={(e) => { setconfirmPassword(e.target.value) }}
+                                value={confirmPassword}
+                                onChange={(e) => { setConfirmPassword(e.target.value) }}
                                 className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
                             />
                         </div>
-
                         {errorMessage && (
-                            <span className='text-red-600 font-bold'>{errorMessage}</span>
+                            <p className='text-red-600 font-bold'>{errorMessage}</p>
                         )}
-
                         <button
                             type="submit"
                             disabled={isRegistering}
@@ -100,7 +98,7 @@ const Register = () => {
                 </div>
             </main>
         </>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
